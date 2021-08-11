1181ee8bbd6d60fd089a5f13010119165bf864e8
Eduardo Gautier <duduchn2011@gmail.com>
2021-08-10 23:29:28 -0300

    Exercicios Java

--- ProjArrayIdade/src/projarrayidade/Main.java
+++ ProjArrayIdade/src/projarrayidade/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        
+         
         byte[] idades = {16,17,16,16,17,17,27,19,16,18,17,19,16,15};
         
         System.out.println(Arrays.toString(idades));
